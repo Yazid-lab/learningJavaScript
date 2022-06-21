e6e262873a3da776bf13984edd7f26ac00325501
Bougrine <yazidbougrine99@gmail.com>
2022-06-21 13:38:02 +0100

    numeric conversion

--- typeConversion.js
+++ typeConversion.js
@@ -4,3 +4,16 @@ console.log(typeof(value));
 
 value=String(value);
 console.log(typeof(value));
+
+//numeric conversion
+//It happens in mathematical functinos and expression automatically
+console.log("6" / "2"); //Output :3. Strings are converted to numbers
+let str="123";
+console.log(typeof(str)); // string
+let number= Number(str); // become a number
+console.log(typeof(number)); // number
+console.log(Number("   123   "));// 123
+console.log(Number("123z"));// NaN
+console.log(Number(true));// 1
+console.log(Number(false));// 0
+
